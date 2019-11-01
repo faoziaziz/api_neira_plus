623bf51df1e4c924db6416cb619824517a21e91e
Aziz Faozi <admin@labseni.com>
2019-11-01 16:01:10 +0700

    rasengga

--- api/controller/tester.js
+++ api/controller/tester.js
@@ -17,9 +17,8 @@ exports.testerGet3 = function(req, res, next){
     var param2 = req.param('param2');
     var UUID_gen  = uuidv1();
 
-    
-    res.send("param1 : "+param1);
-    console.log("the blob data :"+param2);
+
+   
     /*write to db tester*/
     connection.connnect(function (err){
         console.log("weew");
@@ -35,7 +34,11 @@ exports.testerGet3 = function(req, res, next){
             console.log("Record berhasil ");
         });
 
+
     });
+    
+    res.send("param1 : "+param1);
+    console.log("the blob data :"+param2);
     
 }
 
